67315043246d9c330bfbef9d8c4d69a727e1a24b
Shivam Choudhary <60214221+shivammchaudhary@users.noreply.github.com>
2023-07-23 21:02:24 -0400

    Add files via upload

---
+++ deploy.js
@@ -0,0 +1,18 @@
+// scripts/deploy.js
+
+async function main() {
+    const HelloWorldFactory = await ethers.getContractFactory("HelloWorld")
+    const helloMessage = await HelloWorldFactory.deploy("Hello from QuickNode")
+    await helloMessage.deployed()
+  
+    console.log("Contract deployed to:", helloMessage.address)
+    console.log("Contract deployed by " + JSON.stringify(helloMessage.signer) + " signer")
+    process.exit(0)
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error)
+      process.exit(1)
+    })
